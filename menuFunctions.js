5368413af573d475119d932ee1d7e6ef517bfad6
magilla121 <liev.haroche@sonomaacademy.org>
2018-03-08 14:29:14 -0800

    HELLO!

---
+++ menuFunctions.js
@@ -0,0 +1,24 @@
+module.exports = {
+  saveAs: saveAs
+}
+
+const {BrowserWindow} = require('electron');
+const url = require('url');
+
+let saveWindow;
+
+function saveAs(BW) {
+  // Create the save browser window.
+  saveWindow = new BW({width: 700, height: 400});
+  //saveWindow.setMenu(null);
+  saveWindow.loadURL(url.format({
+    pathname: (__dirname + '/public/saveWindow/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  saveWindow.on('closed', function () {
+    saveWindow = null;
+  });
+
+}
---
+++ public/saveWindow/script.js
@@ -0,0 +1,46 @@
+//imports
+const {ipcRenderer} = require('electron');
+const {remote} = require('electron');
+//fs and path modules are used but not imported here because they come with the userEdit.js which is imported by the saveWindow index, how nice!
+var saveAs = document.getElementById('saveAs');
+
+/* as soon as the window opens, this script reads the userData.json file to see where the last file was saved.
+If this is the first time, the program assumes the directory is just Spud/Files/(filename).fileformat
+Note:In the future this will probably change to Documents/SpudProjects or something like that
+*/
+getFavoriteDirectory((favDir) => {
+  console.log(favDir);
+  //checks to make sure that the lastDirectory actually exists, if it does, assume the user wants to save their again
+  if(favDir != "") {
+    console.log('t');
+    saveAs.value = favDir;
+  } else {
+    //if the user has never saved a file through spud before, assume they want to use the default path of TextDitor/Files
+    saveAs.value =  ('../../../TextEditor/Files/hello.txt');
+  }
+});
+
+//listen carefully!
+document.addEventListener('keydown', (event) => {
+//when user presses 'enter', save the fileData
+  if(event.keyCode === 13) {
+    saveFile();
+  }
+});
+
+var saveFile = () => {
+  try{
+    var fileName = saveAs.value;
+    setFavoriteDirectory(fileName);
+    ipcRenderer.send('saveRequest', fileName);
+  } catch(e) {
+    alert('Hmm something went wrong...');
+    console.log(e);
+  }
+}
+
+//LOCAL SOCKETS ----------------------------------------------------------------------------------------------------
+ipcRenderer.on('doneSaving', (event) => {
+  remote.getCurrentWindow()
+  .close();
+});
